7d94388de293d4fdc259f248ddfcf6e40ce32456
hema-kun <herupurnama47.hp@gmail.com>
2025-05-27 09:43:21 +0700

    style : fp

--- FP/deklaratif.js
+++ FP/deklaratif.js
@@ -1,6 +1,7 @@
-const names = ['Harry','Ron','Jeff','Thomas'];
+const names = ["Harry", "Ron", "Jeff", "Thomas"];
 
-const newNames = names.map((name)=> `${name}!`);
-
+const newNames = names.map((name) =>{
+    return name;
+})
 
 console.log(newNames);
--- FP/imperatif.js
+++ FP/imperatif.js
@@ -1,12 +1,10 @@
-
-
-const names = ['Harry','Ron','Jeff','Thomas'];
+const names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
 
 const newNames = [];
 
-for(let i=0;i<names.length;i++){
+for (let i = 0; i < names.length; i++) {
     newNames.push(`${names[i]}`)
-    newNames.push(`Elemen ${i+1} di indeks ${i} `)
+   // newNames.push(`Elemen ${i+1} di indeks ${i} `)
 }
 
 console.log(newNames);
